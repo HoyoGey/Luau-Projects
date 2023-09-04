7242338cf37b6499ea28247dba3522254bc10f1c
Mr.Xrer <117149371+HoyoGey@users.noreply.github.com>
2023-09-04 14:54:20 +0300

    Update init.lua

--- test/init.lua
+++ test/init.lua
@@ -1,2 +1,33 @@
-require("functions.lua")
-require("ui.lua")
+_G.YodeW = {}
+local storage = _G.YodeW
+function registerStorage(path: string)
+    local paths = string.split(path, "/")
+    local endFile = paths[#paths]
+    local endFileType = string.split(endFile, ".")[2]
+    local currentStorage = storage
+    
+    for _, path_split in paths do
+        if path_split == "" then continue end
+        
+        local name = string.split(path_split, ".")[1]
+        currentStorage[name] = currentStorage[name] or {}
+        
+        if path_split == endFile and endFileType == 'lua' then
+            currentStorage[name] = _G.YodeW.LoadHttp(urlGitHub .. path)
+        end
+        
+        currentStorage = currentStorage[name]
+    end
+end
+
+_G.YodeW.registerStorage = registerStorage
+
+_G.YodeW.LoadHttp = function(path: string)
+	return loadstring(game:HttpGet(path))()
+end
+
+_G.YodeW.requireGit = registerStorage
+
+
+registerStorage("/functions.json")
+registerStorage("/ui.json")
